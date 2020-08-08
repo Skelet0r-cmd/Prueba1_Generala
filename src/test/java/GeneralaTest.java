ad78b302350b8bd6cfeaf642700e18c8b86d2613
Carlos Castillo 1 <c.castillo15@ufromail.cl>
2020-08-07 21:15:18 -0400

    Se crearon dos métodos extras searhObjectRepeated y searchNumberSpecific, estos se le implementan al cuerpo de largeStraight, haciendo que pase todos los test

--- src/test/java/GeneralaTest.java
+++ src/test/java/GeneralaTest.java
@@ -1,8 +1,11 @@
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+
 import com.mycompany.generala.Generala;
-import org.junit.Ignore;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -16,7 +19,7 @@ import org.junit.Ignore;
  */
 public class GeneralaTest {
 
-    @Ignore
+    
     @Test
     public void chance_scores_sum_of_all_dice() {
         int expected = 15;
@@ -25,7 +28,7 @@ public class GeneralaTest {
         assertEquals(16, Generala.chance(3, 3, 4, 5, 1));
     }
 
-    @Ignore
+    
     @Test
     public void yatzy_scores_50() {
         int expected = 50;
@@ -35,7 +38,7 @@ public class GeneralaTest {
         assertEquals(0, Generala.generala(6, 6, 6, 6, 3));
     }
 
-    @Ignore
+    
     @Test
     public void test_1s() {
         assertTrue(Generala.ones(1, 2, 3, 4, 5) == 1);
@@ -44,21 +47,21 @@ public class GeneralaTest {
         assertEquals(4, Generala.ones(1, 2, 1, 1, 1));
     }
 
-    @Ignore
+    
     @Test
     public void test_2s() {
         assertEquals(4, Generala.twos(1, 2, 3, 2, 6));
         assertEquals(10, Generala.twos(2, 2, 2, 2, 2));
     }
 
-    @Ignore
+    
     @Test
     public void test_threes() {
         assertEquals(6, Generala.threes(1, 2, 3, 2, 3));
         assertEquals(12, Generala.threes(2, 3, 3, 3, 3));
     }
 
-    @Ignore
+    
     @Test
     public void fours_test() {
         assertEquals(12, new Generala(4, 4, 4, 5, 5).fours());
@@ -66,7 +69,7 @@ public class GeneralaTest {
         assertEquals(4, new Generala(4, 5, 5, 5, 5).fours());
     }
 
-    @Ignore
+    
     @Test
     public void fives() {
         assertEquals(10, new Generala(4, 4, 4, 5, 5).fives());
@@ -74,7 +77,7 @@ public class GeneralaTest {
         assertEquals(20, new Generala(4, 5, 5, 5, 5).fives());
     }
 
-    @Ignore
+    
     @Test
     public void sixes_test() {
         assertEquals(0, new Generala(4, 4, 4, 5, 5).sixes());
@@ -82,7 +85,7 @@ public class GeneralaTest {
         assertEquals(18, new Generala(6, 5, 6, 6, 5).sixes());
     }
 
-    @Ignore
+    
     @Test
     public void one_pair() {
         assertEquals(6, Generala.score_pair(3, 4, 3, 5, 6));
@@ -90,14 +93,14 @@ public class GeneralaTest {
         assertEquals(12, Generala.score_pair(5, 3, 6, 6, 5));
     }
 
-    @Ignore
+    
     @Test
     public void two_Pair() {
         assertEquals(16, Generala.two_pair(3, 3, 5, 4, 5));
         assertEquals(16, Generala.two_pair(3, 3, 5, 5, 5));
     }
 
-    @Ignore
+    
     @Test
     public void three_of_a_kind() {
         assertEquals(9, Generala.three_of_a_kind(3, 3, 3, 4, 5));
@@ -105,7 +108,7 @@ public class GeneralaTest {
         assertEquals(9, Generala.three_of_a_kind(3, 3, 3, 3, 5));
     }
 
-    @Ignore
+    
     @Test
     public void four_of_a_knd() {
         assertEquals(12, Generala.four_of_a_kind(3, 3, 3, 3, 5));
@@ -113,7 +116,7 @@ public class GeneralaTest {
         assertEquals(9, Generala.three_of_a_kind(3, 3, 3, 3, 3));
     }
 
-    @Ignore
+    
     @Test
     public void smallStraight() {
         assertEquals(15, Generala.smallStraight(1, 2, 3, 4, 5));
@@ -128,7 +131,7 @@ public class GeneralaTest {
         assertEquals(0, Generala.largeStraight(1, 2, 2, 4, 5));
     }
 
-    @Ignore
+    
     @Test
     public void fullHouse() {
         assertEquals(18, Generala.fullHouse(6, 2, 2, 2, 6));
